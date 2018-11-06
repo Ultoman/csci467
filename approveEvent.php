876474398358f529817532fb0e9f8ed8306d8279
Jacqueline Salim <z1761458@turing.cs.niu.edu>
2018-11-06 00:41:00 -0600

    GEM is not a link

--- approveEvent.php
+++ approveEvent.php
@@ -2,43 +2,17 @@
 //Jackie Salim
 //This allows the user to add an Owner
 
-  $pageTitle = "Add Owner";
+  $pageTitle = "Approve Event";
 
   include("header.html");
   require_once("conn.php");
 
-  echo 'Add a new Owner:<br>';
+  echo '<h1 align="center">'.$pageTitle.'</h1>';
 
   //create form
   echo '<form action="approveEvent.php" method="post">';
 
-  echo '<input type="text" name="fName" id="fName" placeholder="First Name"><br>';
-  echo '<input type="text" name="lName" id="lName" placeholder="Last Name"><br>';
-
-  echo '<br>';
-  echo '<input type="submit" name="submit" value="Add">';
-  echo '&nbsp&nbsp&nbsp';
-  echo '<input type="reset" name="reset">';
-
   echo '</form>';
-
-  //handles button action
-  if ($_SERVER['REQUEST_METHOD'] == 'POST')
-  {
-    $firstname = $_POST['fName'];
-    $lastname = $_POST['lName'];
-
-    $sql = "insert into Owner (FirstName, LastName) values (?, ?)";
-
-    try{
-      $stmt = $conn->prepare($sql);
-      $stmt->execute(array($firstname, $lastname));
-    }
-    catch(PDOException $e){
-       $message = $e->getMessage();
-       echo "<script type='text/javascript'>alert('$message');</script>";
-    }
-  }
 
   include("footer.html");
 ?>

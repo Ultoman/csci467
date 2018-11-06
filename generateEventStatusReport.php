cc2151879d8763dbecd8a6737f593a8d5dd1428b
Jacqueline Salim <z1761458@turing.cs.niu.edu>
2018-11-06 12:29:09 -0600

    james told me to

--- generateEventStatusReport.php
+++ generateEventStatusReport.php
@@ -3,42 +3,100 @@
 //This displays the boats owned by the specified owner. The owner last names
 //are displayed in a drop down menu and when selected and sumbitted, the names of the boats
 //owned by that owner are displayed in a table.
-  $pageTitle = "Owner Boat Information";
+  $pageTitle = "Generate Detail Report";
   include("header.html");
   require_once("conn.php");
-  echo 'Select an Owner:<br>';
+
+ echo '<h1 align="center">'.$pageTitle.'</h1>';
+
   //create form
-  echo '<form action="createEvent.php" method="post">';
+  echo '<form action="generateEventStatusReport.php" method="post">';
   //create the drop down maneu
-  echo '<select name="ownersLastNames">';
-  echo '<option value="empty">-- select an owner --</option>';
+
+  echo '<div align="center">';
+  echo '<label for="events" style="display: block">Select an Event:<label>';
+  echo '<select name="events" style="display: block">';
+  echo '<option value="empty">-- select an Event --</option>';
   //populate drop down with database info
-  foreach($conn->query('SELECT LastName FROM Owner') as $ownersLastNames)
+  foreach($conn->query('SELECT EventName,EventId FROM Event') as $event)
   {
-   echo '<option value="'.$ownersLastNames['LastName'].'">'.$ownersLastNames['LastName'].'</option>';
+   echo '<option value="'.$event['EventId'].'">'.$event['EventName'].'</option>';
   }
   echo '</select>';
+  echo '</div>';
   echo '<br><br>';
-  echo '<input type="submit" name="submit" value="Show">';
-  echo '&nbsp&nbsp&nbsp';
-  echo '<input type="reset" name="reset">';
+
+  echo '<div style="padding: 2em" align="center">';
+    echo '<button type="reset" class="button button1" style="margin: 0 2em">Clear</button>';
+    echo '<button type="submit" class="button button1" style="margin: 0 2em">Create</button>';
+  echo '</div>';
+
   echo '</form>';
+
   //handles button action
   if ($_SERVER['REQUEST_METHOD'] == 'POST')
   {
-   $lastname = $_POST['ownersLastNames'];
-   $sql = "select BoatName from MarinaSlip,Owner where Owner.LastName = '$lastname' and Owner.OwnerId = MarinaSlip.OwnerNum";
-   echo 'Boats owned by '.$lastname.':<br><br>';
-   echo '<table border=1>';
+   $eventId = $_POST['events'];
+   $sql = "select * from Event where EventId = '$eventId'";
+
+  echo '<div style="padding: 2em" align="center">';
+   echo '<table border="1" style="border-collapse: collapse" >';
+    echo '<tr>';
+     echo '<th>';
+      echo 'Id';
+     echo '</th>';
+     echo '<th>';
+      echo 'Name';
+     echo '</th>';
+     echo '<th>';
+      echo 'Date';
+     echo '</th>';
+     echo '<th>';
+      echo 'Start Time';
+     echo '</th>';
+     echo '<th>';
+      echo 'Seating Capacity';
+     echo '</th>';
+     echo '<th>';
+      echo 'Singer';
+     echo '</th>';
+     echo '<th>';
+      echo 'Notes';
+     echo '</th>';
+    echo'</tr>';
    foreach($conn->query($sql) as $row)
    {
     echo '<tr>';
       echo '<td width=100 align="center">';
-        echo $row['BoatName'];
+        echo $row['EventId'];
+      echo '</td>';
+      echo '<td width=100 align="center">';
+        echo $row['EventName'];
+      echo '</td>';
+      echo '<td width=100 align="center">';
+        echo $row['Date'];
+      echo '</td>';
+      echo '<td width=100 align="center">';
+        echo $row['StartTime'];
+      echo '</td>';
+      echo '<td width=100 align="center">';
+        echo $row['SeatingCapacity'];
+      echo '</td>';
+      echo '<td width=100 align="center">';
+        echo $row['Singer'];
+      echo '</td>';
+      echo '<td width=100 align="center">';
+        echo $row['Notes'];
       echo '</td>';
     echo '</tr>';
    }//end for each
    echo '</table>';
+  echo '</div>';
+
+  echo '<div style="padding: 2em" align="center">';
+   echo '<button class="button button1" onClick="window.print()">Print</button>';
+  echo '</div>';
+
   }//end if
   include("footer.html");
 ?>
